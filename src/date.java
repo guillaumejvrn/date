3515271b9a740536b9592332348bf40d47b897c1
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2024-01-25 14:03:57 +0100

    sadge

--- src/date.java
+++ src/date.java
@@ -2,9 +2,16 @@ public class date {
     double jour;
     double mois;
     double an;
-    public date(){
+
+    public date() {
         this.jour = 0;
         this.mois = 0;
         this.an = 0;
+    }
+
+    date(double jour, double mois, double an) {
+        this.jour = jour;
+        this.mois = mois;
+        this.an = an;
     }
 }
