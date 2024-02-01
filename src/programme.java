75f19817251d318d86fa96aa911017880f3eb9d4
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2024-02-01 14:21:24 +0100

    sadge

--- src/programme.java
+++ src/programme.java
@@ -1,9 +1,5 @@
 public class programme {
     public static void main(String[] args) {
-        System.out.println("ça marche, c'est parfait.");
-        date date1 = new date(1, 10, 2024);
-        System.out.println("jour = "+date1.jour);
-        System.out.println("mois = "+date1.mois);
-        System.out.println("an = "+date1.an);
+        System.out.println("Hello World");
     }
 }
